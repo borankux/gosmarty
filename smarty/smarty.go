98d536a6ff05755f372e3a9ae4d8f6abc2f81567
Frank <mablat@ebay.com>
2021-07-23 14:26:25 +0800

    add file replacing

--- smarty/smarty.go
+++ smarty/smarty.go
@@ -3,6 +3,8 @@ package smarty
 import (
 	"fmt"
 	"io/ioutil"
+	"regexp"
+	"strings"
 )
 
 type Smarty struct {
@@ -22,10 +24,41 @@ func (s *Smarty) Print() {
 	}
 }
 
+func Parse(html string, vars map[string]interface{}) string {
+	rule := `[{]{2}[$][a-zA-Z_]+[a-zA-Z0-9_]+[}]{2}`
+	r, _ := regexp.Compile(rule)
+	found := r.FindAllString(html, -1)
+	for _, f := range found {
+		key:= getKey(f)
+		if val, ok := vars[key]; ok {
+			krule := `[{]{2}[$]` + key + `+[}]{2}`
+			kr, _ := regexp.Compile(krule)
+			html = kr.ReplaceAllString(html, fmt.Sprintf("%v", val))
+		}
+	}
+	return html
+}
+
+func getKey(matched string) string {
+	matched = strings.Replace(matched, "{{", "", -1)
+	matched = strings.Replace(matched, "}}", "", -1)
+	return strings.Replace(matched, "$", "", 1)
+}
+
 func (s *Smarty) Render(fileName string)  string {
 	f, err := ioutil.ReadFile(fmt.Sprintf("templates/%s.tpl", fileName))
 	if err != nil {
 		panic(err)
 	}
-	return Parse(string(f), s.vars)
+	return Parse(s.Inflate(string(f)), s.vars)
+}
+
+func (s *Smarty) Inflate(html string) string {
+	rule := ``
+	r,_ := regexp.Compile(rule)
+	all := r.FindAllString(html, -1)
+	for _, s:= range all {
+		fmt.Println(s)
+	}
+	return html
 }
---
+++ templates/include-file.tpl
@@ -0,0 +1,3 @@
+<div class="father">
+   {include file="child"}
+</div>
