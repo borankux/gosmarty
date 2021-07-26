c3edd52f8e2929d5c91183d4d142319e906fd5f6
FrankWindows <mirzatsoft@163.com>
2021-07-26 14:23:24 +0800

    rules

--- smarty/rules.go
+++ smarty/rules.go
@@ -3,4 +3,5 @@ package smarty
 const (
 	RULE_INCLUDE string = `(\{)(\s+|)include\s+file(\s+|)=(\s+|)\"([a-zA-Z-\/])+\"(\s+|)\}`
 	RULE_VARIABLE string = `[{]{2}[$][a-zA-Z_]+[a-zA-Z0-9_]+[}]{2}`
+	INCLUDE_ALLMATCH = `(\{\s?include\s?)(([a-z\s]+)=\s?"([a-z\-]*)")*(\})`
 )
